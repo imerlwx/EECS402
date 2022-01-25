685a1f93e36c17b86f848761387417b4b0a53c20
Wengxi Li <72551690+imerlwx@users.noreply.github.com>
2022-01-24 22:55:20 -0500

    Add files via upload

--- Project1/project1.cpp
+++ Project1/project1.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 /* Programmer: Wengxi Li. Date: January 2022.
-Purpose: This file is used to appoximate the area between a curve and the X-axis 
-by fitting many rectangles between the curve and axis, whose midpoints are on 
-the curve. Users could choose any interval, range and tolerance they want. */
+Purpose: This file is used to appoximate the area between a curve and the  
+X-axis by fitting many rectangles between the curve and axis, whose midpoints 
+are on the curve. Users could choose any interval, range and precision they 
+want. And choose whether to calculate the integral or get the number of 
+rectangles that achieve the precision*/
 
 // prints out the possible choices the user has, in a menu format
 void printMenu(); 
@@ -12,8 +14,9 @@ void printMenu();
 // raises "val" to the power "power" and returns the result.
 double toThePower(const double val, const int power); 
 
-/* evaluates the cubic formula's result's sign. return false if the resulting y value
- is a negative value, or true if the resulting value is greater or equal to zero */
+/* evaluates the cubic formula's result's sign. return false if the resulting y
+ value is a negative value, or true if the resulting value is greater or equal 
+ to zero */
 bool evaluateCubicFormula(const double aCoeff, const double bCoeff, 
 const double cCoeff, const double dCoeff, const double xValue, double 
 &resultVal);
@@ -29,112 +32,137 @@ const double endX, const int numRects);
 #else 
 int main()
 {
-    printMenu();
-    
-    int userChoice;
-    cout << "YOUR CHOICE: ";
-    cin >> userChoice;    
+  printMenu();
+  
+  int userChoice;
+  cout << "YOUR CHOICE: ";
+  cin >> userChoice;    
 
-    while (userChoice != 3)
+  while (userChoice != 3)
+  {
+    double aCoeff, bCoeff, cCoeff, dCoeff;
+    cout << "Enter (a b c d) for function y = a*x^3 + b*x^2 + c*x + d: ";
+    cin >> aCoeff >> bCoeff >> cCoeff >> dCoeff;
+
+    double startX, endX;
+    cout << "Now enter x start and end values: ";
+    cin >> startX >> endX;
+
+    if (userChoice == 1)
     {
-        double aCoeff, bCoeff, cCoeff, dCoeff;
-        cout << "Enter (a b c d) for function y = a*x^3 + b*x^2 + c*x + d: ";
-        cin >> aCoeff >> bCoeff >> cCoeff >> dCoeff;
+    int numRects;
+    cout << "Enter the number of rectangles to use: ";
+    cin >> numRects;
 
-        double startX, endX;
-        cout << "Now enter x start and end values: ";
-        cin >> startX >> endX;
-
-        if (userChoice == 1)
-        {
-        int numRects;
-        cout << "Enter the number of rectangles to use: ";
-        cin >> numRects;
-
-        cout << "Rectangle result is: " 
-             << approximateAreaWithRectangles(aCoeff, bCoeff, cCoeff, dCoeff, startX, endX, numRects)
-             << endl;
-
-        }
-        else if (userChoice == 2)
-        {
-        double correctAnswer;
-        cout << "Enter correct answer: ";
-        cin >> correctAnswer;
-
-        double precision;
-        cout << "Enter precision to reach: ";
-        cin >> precision;
-
-        int numRects = 1;
-        while (numRects <= 100 && 
-        (correctAnswer - approximateAreaWithRectangles(aCoeff, bCoeff, cCoeff, dCoeff, 
-        startX, endX, numRects)) > precision)
-        {
-            numRects++;
-        }
-        
-        if (numRects <= 100)
-            cout << "Rectangles needed to reach precision: " << numRects << endl;
-        else
-            cout << "Tried 100 rectangles without reaching precision" << endl;
-
-        }
-        printMenu();
-        cin >> userChoice;  
+    cout << "Rectangle result is: " 
+         << approximateAreaWithRectangles(aCoeff, bCoeff, cCoeff, dCoeff, 
+         startX, endX, numRects)
+         << endl;
     }
     
-    cout << "Thanks for using this program";
-    return 0;           
+    else if (userChoice == 2)
+    {
+    double correctAnswer;
+    cout << "Enter correct answer: ";
+    cin >> correctAnswer;
+
+    double precision;
+    cout << "Enter precision to reach: ";
+    cin >> precision;
+
+    int numRects = 1;
+    while (numRects <= 100 && 
+    (correctAnswer - approximateAreaWithRectangles(aCoeff, bCoeff, cCoeff, 
+    dCoeff, startX, endX, numRects)) > precision) 
+    // either numRects is larger than 100 or the difference is smaller than 
+    // precision will the while loop end
+    {
+      numRects++;
+    }        
+    
+    if (numRects <= 100)
+    {
+      cout << "Rectangles needed to reach precision: " << numRects << endl;
+    }        
+    else
+    {
+      cout << "Tried 100 rectangles without reaching precision" << endl;
+    }        
+    }
+    
+    printMenu();
+    cin >> userChoice;  // until user input 3, the loop will not quit
+  }
+  
+  cout << "Thanks for using this program";
+  return 0;           
 }
 #endif 
 
 void printMenu() 
 {
-    cout << "1 Approximate Integral Using Rectangles" << endl;
-    cout << "2 Experiment With Rectangle Precision" << endl;
-    cout << "3 Quit The Program" << endl;
+  cout << "1 Approximate Integral Using Rectangles" << endl;
+  cout << "2 Experiment With Rectangle Precision" << endl;
+  cout << "3 Quit The Program" << endl;
 }
 
 double toThePower(const double val, const int power) 
 {
-    double powerResult = 1; // initialize the power result
+  double powerResult = 1; // initialize the power result for power = 0
 
-    for (int i = 0; i < power; i++)
-        powerResult *= val;
-
-    return powerResult;
+  for (int i = 0; i < power; i++)
+  {
+    powerResult *= val;
+  }      
+  return powerResult;
 }
 
 bool evaluateCubicFormula(const double aCoeff, const double bCoeff, 
 const double cCoeff, const double dCoeff, const double xValue, double 
 &resultVal) 
 {
-    resultVal = aCoeff * toThePower(xValue, 3) + bCoeff * toThePower(xValue, 2) + cCoeff * xValue + dCoeff;
-    if (resultVal < 0)
-        return false;
-    else
-        return true;
+  // this is the cubic formula y = aCoeff * xValue^3 + bCoeff * xValue^2 + 
+  //cCoeff * xValue + dCoeff
+  resultVal = aCoeff * toThePower(xValue, 3) + bCoeff * toThePower(xValue, 2) +
+   cCoeff * xValue + dCoeff;
+  
+  if (resultVal < 0) // evaluate whether resultVal is negative or positive
+  {
+    return false;
+  }      
+  else
+  {
+    return true;
+  }      
 }
 
 double approximateAreaWithRectangles(const double aCoeff, const double 
 bCoeff, const double cCoeff, const double dCoeff, const double startX, 
 const double endX, const int numRects)
 {
-    double recWidth = (endX - startX) / numRects; // the rectangle's width
-    double approArea = 0; // initialize the approxime area under the curve
-    double xValue = startX + recWidth / 2; // the initial xValue equals startX plus half of the rectangle width
+  double recWidth = (endX - startX) / numRects; // the rectangle's width
+  double approArea = 0; // initialize the approxime area under the curve
+  
+  // the initial xValue equals startX plus half of the rectangle width
+  double xValue = startX + recWidth / 2; 
 
-    for (int i = 0; i < numRects; i++)
-    {        
-        double recHeight = aCoeff * toThePower(xValue, 3) + bCoeff * toThePower(xValue, 2) + cCoeff * xValue + dCoeff;
-        
-        if (evaluateCubicFormula(aCoeff, bCoeff, cCoeff, dCoeff, xValue, recHeight))
-            approArea += recHeight * recWidth; // add all the rectangles' area to get the area under the curve
-        else
-            approArea -= recHeight * recWidth; // when the area is negative, we should add its absolute value
-        
-        xValue += recWidth; // move to the midpoint of the next rectangle
-    }
-    return approArea;
+  for (int i = 0; i < numRects; i++)
+  {        
+    double recHeight = aCoeff * toThePower(xValue, 3) + bCoeff * 
+    toThePower(xValue, 2) + cCoeff * xValue + dCoeff;
+    
+    if (evaluateCubicFormula(aCoeff, bCoeff, cCoeff, dCoeff, xValue, recHeight))
+    {
+      // add all the rectangles' area to get the area under the curve
+      approArea += recHeight * recWidth; 
+    }  
+    else
+    {
+      // when the area is negative, we should add its absolute value
+      approArea -= recHeight * recWidth; 
+    }  
+
+    xValue += recWidth; // move to the midpoint of the next rectangle
+  }
+  return approArea;
 }
