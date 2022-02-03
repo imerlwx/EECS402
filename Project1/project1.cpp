1d0b5be7901e5877e8863d1e229618b3ea37ed1c
Wengxi Li <72551690+imerlwx@users.noreply.github.com>
2022-02-03 10:25:37 -0500

    Add files via upload

--- Project1/project1.cpp
+++ Project1/project1.cpp
@@ -31,14 +31,18 @@ const double endX, const int numRects);
 #include "andrewTest.h" 
 #else 
 int main()
-{
-  printMenu();
+{ 
+  const int MAX_NUM_RECTANGLES = 100;
+  const int APPR_INTEGRAL = 1;
+  const int EXPERI_PRECISION = 2;
+  const int QUIT = 3;
   
+  printMenu(); 
   int userChoice;
   cout << "YOUR CHOICE: ";
   cin >> userChoice;    
 
-  while (userChoice != 3)
+  while (userChoice != QUIT)
   {
     double aCoeff, bCoeff, cCoeff, dCoeff;
     cout << "Enter (a b c d) for function y = a*x^3 + b*x^2 + c*x + d: ";
@@ -48,49 +52,51 @@ int main()
     cout << "Now enter x start and end values: ";
     cin >> startX >> endX;
 
-    if (userChoice == 1)
+    if (userChoice == APPR_INTEGRAL)
     {
-    int numRects;
-    cout << "Enter the number of rectangles to use: ";
-    cin >> numRects;
+      int numRects;
+      cout << "Enter the number of rectangles to use: ";
+      cin >> numRects;
 
-    cout << "Rectangle result is: " 
-         << approximateAreaWithRectangles(aCoeff, bCoeff, cCoeff, dCoeff, 
-         startX, endX, numRects)
-         << endl;
+      cout << "Rectangle result is: " 
+           << approximateAreaWithRectangles(aCoeff, bCoeff, cCoeff, dCoeff, 
+           startX, endX, numRects)
+           << endl;
     }
     
-    else if (userChoice == 2)
+    else if (userChoice == EXPERI_PRECISION)
     {
-    double correctAnswer;
-    cout << "Enter correct answer: ";
-    cin >> correctAnswer;
+      double correctAnswer;
+      cout << "Enter correct answer: ";
+      cin >> correctAnswer;
 
-    double precision;
-    cout << "Enter precision to reach: ";
-    cin >> precision;
+      double precision;
+      cout << "Enter precision to reach: ";
+      cin >> precision;
 
-    int numRects = 1;
-    while (numRects <= 100 && 
-    (correctAnswer - approximateAreaWithRectangles(aCoeff, bCoeff, cCoeff, 
-    dCoeff, startX, endX, numRects)) > precision) 
-    // either numRects is larger than 100 or the difference is smaller than 
-    // precision will the while loop end
-    {
-      numRects++;
-    }        
-    
-    if (numRects <= 100)
-    {
-      cout << "Rectangles needed to reach precision: " << numRects << endl;
-    }        
-    else
-    {
-      cout << "Tried 100 rectangles without reaching precision" << endl;
-    }        
+      int numRects = 1;
+      while (numRects <= MAX_NUM_RECTANGLES && 
+        abs(correctAnswer - approximateAreaWithRectangles(aCoeff, bCoeff, 
+        cCoeff, dCoeff, startX, endX, numRects)) > precision) 
+      // either numRects is larger than MAX_NUM_RECTANGLES or the difference
+      // is smaller than precision will the while loop end
+      {
+        numRects++;
+      }        
+      
+      if (numRects <= MAX_NUM_RECTANGLES)
+      {
+        cout << "Rectangles needed to reach precision: " << numRects << endl;
+      }        
+      else
+      {
+        cout << "Tried" << MAX_NUM_RECTANGLES 
+             << "rectangles without reaching precision" << endl;
+      }        
     }
     
     printMenu();
+    cout << "YOUR CHOICE: ";
     cin >> userChoice;  // until user input 3, the loop will not quit
   }
   
@@ -122,7 +128,7 @@ const double cCoeff, const double dCoeff, const double xValue, double
 &resultVal) 
 {
   // this is the cubic formula y = aCoeff * xValue^3 + bCoeff * xValue^2 + 
-  //cCoeff * xValue + dCoeff
+  // cCoeff * xValue + dCoeff
   resultVal = aCoeff * toThePower(xValue, 3) + bCoeff * toThePower(xValue, 2) +
    cCoeff * xValue + dCoeff;
   
@@ -149,7 +155,7 @@ const double endX, const int numRects)
   for (int i = 0; i < numRects; i++)
   {        
     double recHeight = aCoeff * toThePower(xValue, 3) + bCoeff * 
-    toThePower(xValue, 2) + cCoeff * xValue + dCoeff;
+      toThePower(xValue, 2) + cCoeff * xValue + dCoeff;
     
     if (evaluateCubicFormula(aCoeff, bCoeff, cCoeff, dCoeff, xValue, recHeight))
     {
